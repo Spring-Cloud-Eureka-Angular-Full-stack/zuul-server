cc36568c0b6b9c52678d6503c9e23e3077451354
Luis Guisado <luis.guisado.matrix@gmail.com>
2023-01-26 01:26:05 -0500

    First commit

---
+++ src/test/java/com/lagm/microservices/app/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagm.microservices.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
